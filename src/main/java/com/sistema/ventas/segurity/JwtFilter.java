2c073761ff7c44231908834f08b12953c84554c6
Bryan <bryan.zamora@goitsa.me>
2020-10-15 22:28:08 -0500

    Ajuste de autenticacion

--- src/main/java/com/sistema/ventas/segurity/JwtFilter.java
+++ src/main/java/com/sistema/ventas/segurity/JwtFilter.java
@@ -1,16 +1,19 @@
 package com.sistema.ventas.segurity;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.core.Response.Status;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -34,35 +37,46 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException{
 	    try {
 	        String authorizationHeader = httpServletRequest.getHeader("Authorization");
-	
+	        String[] arrServiciosSinSeguridad = { "/ventas/v1/tipoIdentificacion" };	
+	        
 	        String token = null;
 	        String userName = null;
 	
-		        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-		            token = authorizationHeader.substring(7);
-					userName = jwtUtil.extractUsername(token);
-		        }
-		
-		        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-		
-		            UserDetails userDetails = service.loadUserByUsername(userName);
-		
-		            if (jwtUtil.validateToken(token, userDetails)) {
-		
-		                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-		                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+	        if (!(httpServletRequest.getRequestURI()!=null && 
+					Arrays.stream(arrServiciosSinSeguridad).anyMatch(StringUtils.upperCase(httpServletRequest.getRequestURI())::equalsIgnoreCase))) {
+		        	
+		        	 if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+				            token = authorizationHeader.substring(7);
+							userName = jwtUtil.extractUsername(token);
+				        }
+				
+				        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+				
+				            UserDetails userDetails = service.loadUserByUsername(userName);
+				
+				            if (jwtUtil.validateToken(token, userDetails)) {
+				
+				                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+				                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				                usernamePasswordAuthenticationToken
+				                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+				                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+				            }
+				        }
+		        }else {
+		        	 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+		                        new UsernamePasswordAuthenticationToken(null, null,null);
 		                usernamePasswordAuthenticationToken
 		                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
 		                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-		            }
 		        }
+		       
 		        
 			} catch (Exception e) {
 				e.printStackTrace();
 				httpServletResponse.setStatus(Status.UNAUTHORIZED.getStatusCode());
 				httpServletResponse.setHeader("msgAutorizadorException", e.getMessage());
 				logger.log(Level.SEVERE, null, e);
-				System.out.print("==========>"+ e.getMessage());
 				throw new ClientErrorException(e.getMessage()!=null?e.getMessage():"Exception SecurityRequestFilter: ", Status.UNAUTHORIZED ,e);
 			}
 	    filterChain.doFilter(httpServletRequest, httpServletResponse);
